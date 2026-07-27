7cd79736fbcb3e596a3483208b1d77af7eeaebf2
Ugochukwu Ndukwe <ugoemmanuel13@gmail.com>
2026-07-27 15:42:52 -0700

    fix: remove stray brace from base.py

--- abia-app/abia/settings/base.py
+++ abia-app/abia/settings/base.py
@@ -115,8 +115,6 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
-}
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
